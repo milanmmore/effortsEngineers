f8fa23cd8273f052c2512844441c6e3c12773ae9
Milanmmore <milanmmore@gmail.com>
2026-09-17 03:42:29 +0530

    deploy: release frontend v1.0.3 with lowercase basePath and proper formatting

---
+++ _next/static/3vbAYkpwaqFUVkEkF9WWj/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/effortsEngineers//_next/:path+",
+        "destination": "/effortsEngineers/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
